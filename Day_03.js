408dc35cca965a06c33efce17e88eda7a7a622c7
marieham45 <102411896+marieham45@users.noreply.github.com>
2022-12-03 14:23:48 +0100

    Update Day_03.js

--- Day_03.js
+++ Day_03.js
@@ -201,5 +201,203 @@ const findSameValues = (arr) => {
     return sum;
 
 }
+const groupOfThree = (arr) => {
+    const groupSize = 3;
+    let arrayOfGroups = [];
+for (let i = 0; i < arr.length; i += groupSize) {
+    const group = arr.slice(i, i + groupSize);
+    arrayOfGroups.push(group);
+}
+    return arrayOfGroups;
+}
 
-console.log(findSameValues(splitInHalf(testArray)))
+const findBadge = (arr) => {
+    let sameItemsString = "";
+    let sameValue = "";
+    for (let i = 0; i < arr.length; i++) {
+        for (let m = 0; m < arr[i][0].length; m++) {
+            if (arr[i][1].includes(arr[i][0][m]) && arr[i][2].includes(arr[i][0][m])) {
+                sameValue = arr[i][0][m];
+            }
+             
+
+        }
+    sameItemsString += sameValue;
+
+    }
+                        
+    
+        let sum = 0;
+
+
+    for (let j = 0; j < sameItemsString.length; j++) {
+
+    switch (sameItemsString[j]) {
+            case "a":
+            sum += 1;
+            break;
+            case "b":
+            sum += 2;
+            break;
+            case "c":
+            sum += 3;
+            break;
+            case "d":
+            sum += 4;
+            break;
+            case "e":
+            sum += 5;
+            break;
+            case "f":
+            sum += 6;
+            break;
+            case "g":
+            sum += 7;
+            break;
+            case "h":
+            sum += 8;
+            break;
+            case "i":
+            sum += 9;
+            break;
+            case "j":
+            sum += 10;
+            break;
+            case "k":
+            sum += 11;
+            break;
+            case "l":
+            sum += 12;
+            break;
+            case "m":
+            sum += 13;
+            break;
+            case "n":
+            sum += 14;
+            break;
+            case "o":
+            sum += 15;
+            break;
+            case "p":
+            sum += 16;
+            break;
+            case "q":
+            sum += 17;
+            break;
+            case "r":
+            sum += 18;
+            break;
+            case "s":
+            sum += 19;
+            break;
+            case "t":
+            sum += 20;
+            break;
+            case "u":
+            sum += 21;
+            break;
+            case "v":
+            sum += 22;
+            break;
+            case "w":
+            sum += 23;
+            break;
+            case "x":
+            sum += 24;
+            break;
+            case "y":
+            sum += 25;
+            break;
+            case "z":
+            sum += 26;
+            break;
+            case "A":
+            sum += 27;
+            break;
+            case "B":
+            sum += 28;
+            break;
+            case "C":
+            sum += 29;
+            break;
+            case "D":
+            sum += 30;
+            break;
+            case "E":
+            sum += 31;
+            break;
+            case "F":
+            sum += 32;
+            break;
+            case "G":
+            sum += 33;
+            break;
+            case "H":
+            sum += 34;
+            break;
+            case "I":
+            sum += 35;
+            break;
+            case "J":
+            sum += 36;
+            break;
+            case "K":
+            sum += 37;
+            break;
+            case "L":
+            sum += 38;
+            break;
+            case "M":
+            sum += 39;
+            break;
+            case "N":
+            sum += 40;
+            break;
+            case "O":
+            sum += 41;
+            break;
+            case "P":
+            sum += 42;
+            break;
+            case "Q":
+            sum += 43;
+            break;
+            case "R":
+            sum += 44;
+            break;
+            case "S":
+            sum += 45;
+            break;
+            case "T":
+            sum += 46;
+            break;
+            case "U":
+            sum += 47;
+            break;
+            case "V":
+            sum += 48;
+            break;
+            case "W":
+            sum += 49;
+            break;
+            case "X":
+            sum += 50;
+            break;
+            case "Y":
+            sum += 51;
+            break;
+            case "Z":
+            sum += 52;
+            break;
+        default: console.log("default")
+
+    }
+
+    }
+    return sum;
+
+}
+
+console.log(findSameValues(splitInHalf(testArray)));
+console.log(findBadge(groupOfThree(testArray)));
+
